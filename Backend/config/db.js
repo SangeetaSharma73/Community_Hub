9abd992cb6fe40c9a95ec081892f0de649aecb05
Sangeeta <sangeeta136428sharma@gmail.com>
2025-07-03 15:14:16 +0530

    backend updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,16 +1,12 @@
-// config/db.js
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected successfully!");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
-    process.exit(1); // exit the process if connection fails
+    process.exit(1); // Exit the process if connection fails
   }
 };
 
